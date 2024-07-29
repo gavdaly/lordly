2cb1ce15ebabbbc25c31506005baba1900d46469
Gavin Daly <gav.d@me.com>
2024-07-29 14:08:09 -0400

    rename `mod` add fill styles

--- app/src/data_type/mod.rs
+++ app/src/data_type/mod.rs
@@ -1,9 +1,9 @@
 pub mod button_type;
+pub mod color;
 pub mod fill;
 pub mod shape;
-pub mod style;
 
 pub use button_type::ButtonType;
+pub use color::Color;
 pub use fill::Fill;
 pub use shape::Shape;
-pub use style::Color;
