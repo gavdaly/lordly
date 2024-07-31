a81dc740f76ac58c38fe18d9f4176d49ffea2512
Gavin Daly <gav.d@me.com>
2024-07-30 21:44:05 -0400

    feat: Update Indicator component to include position prop and separate children into container

--- app/src/components/indicator.rs
+++ app/src/components/indicator.rs
@@ -1,19 +1,22 @@
-use crate::data_type::{Color, Fill, Shape};
+use crate::data_type::{Color, Fill, Position, Shape};
 use leptos::*;
 
 /// A higher order component that wraps its children in an `span` tag, it shows a colored dot, empty or with a number.
+///
+///
 #[component]
 pub fn Indicator(
     #[prop(optional)] number: usize,
     children: Children,
-    shape: Shape,
-    fill: Fill,
-    color: Color,
+    #[prop(default = "round".into(), into)] shape: Shape,
+    #[prop(default="solid".into(), into)] fill: Fill,
+    #[prop(default="primary".into(), into)] color: Color,
+    #[prop(default="top-left".into(), into)] position: Position,
 ) -> impl IntoView {
     view! {
-        <span>
-            <i class="indicator" data-shape=shape data-fill=fill data-color=color data-number=number>{number}</i>
-            {children()}
+        <span class="indicator-container">
+            <i class="indicator" data-position=position data-shape=shape data-fill=fill data-color=color data-number=number>{number}</i>
+            <span class="indicator-children">{children()}</span>
         </span>
     }
 }
