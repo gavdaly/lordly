dce33e2952a2c6c61cc1ceef7aa556329b6b011c
Gavin Daly <gav.d@me.com>
2024-11-20 14:25:21 -0500

    feat: enhance validation state management in input component
    Add a new `Empty` state to the `ValidationState` enum and implement 
    `FromStr` and `Display` traits for better state handling. Update theinput component to initialize the validation state as `Empty` and 
    refactor validation logic to handle state transitions more clearly. 
    Improve error handling and display messages on the current 
    validation state.

--- app/src/components/input/input.rs
+++ app/src/components/input/input.rs
@@ -21,8 +21,8 @@ pub fn Input(
     #[prop(into)] name: String,
     #[prop(into)] label: String,
     #[prop(into, optional)] summary: Option<String>,
-    #[prop(optional)] placeholder: Option<String>,
-    #[prop(default=(|_|{true}).into(), into)] validation: Callback<String, bool>,
+    #[prop(into, optional)] placeholder: Option<String>,
+    #[prop(default=(|_|{Ok(())}).into(), into)] validation: Callback<String, Result<(), String>>,
     #[prop(default="".into(), into)] wrapper_class: String,
     #[prop(default="".into(), into)] input_class: String,
     #[prop(default="".into(), into)] label_class: String,
@@ -30,54 +30,51 @@ pub fn Input(
     #[prop(default="".into(), into)] error_class: String,
     #[prop(default="✅".into_view(), into)] validation_children: View,
 ) -> impl IntoView {
-    let (validation_state, set_validation_state) =
-        create_signal::<ValidationState>(ValidationState::Dirty);
+    let (state, set_state) = create_signal::<ValidationState>(ValidationState::Empty);
+
     let validate = move |ev: FocusEvent| {
-        let target = ev.target().unwrap();
-        let value = target.value_of().as_string().unwrap();
-        let value = value.clone();
-        match validation(value) {
-            true => set_validation_state(ValidationState::Valid),
-            false => set_validation_state(ValidationState::Invalid("reason".to_string())),
-        };
+        if let Some(target) = ev.target() {
+            match target.value_of().as_string() {
+                Some(v) => match validation(v) {
+                    Ok(_) => set_state(ValidationState::Valid),
+                    Err(err) => set_state(ValidationState::Invalid(err)),
+                },
+                None => set_state(ValidationState::Invalid("Failed to get value".into())),
+            }
+        }
     };
-    let focused = move |_| set_validation_state(ValidationState::Dirty);
+
+    let focused = move |_| set_state(ValidationState::Dirty);
+    let is_invalid = move |state| match state {
+        ValidationState::Invalid(_) => true,
+        _ => false,
+    };
+    let error_message_view = move || match state() {
+        ValidationState::Invalid(message) => view! { <span class="error-message">{message}</span> },
+        ValidationState::Valid => {
+            view! {<span class="success-message">{validation_children}</span>}
+        }
+        _ => view! { <span></span> },
+    };
 
     view! {
-        <div class=format!("input {wrapper_class}")>
-            <div class=format!("summary {summary_class}")>{summary}</div>
-            <label class=format!("input_label {label_class}") for=name.clone()>
-                {label}
-            </label>
+        <div class={format!("input {wrapper_class}")}>
+            <label for=name.clone() class={label_class}>{label}</label>
             <input
-                class=input_class
                 type="text"
-                name=name.clone()
-                id=name
+                name
+                id=name.clone()
                 placeholder=placeholder
                 on:blur=validate
                 on:focus=focused
+                class={format!("input-field {} {}", state().to_string(), input_class)}
+                aria-invalid={is_invalid(state()).to_string()}
+                aria-describedby={format!("{name}-error")}
             />
-            {move || match validation_state() {
-                ValidationState::Invalid(r) => {
-                    view! {
-                        <div class=format!("{error_class}") data-type="error">
-                            {r}
-                        </div>
-                    }
-                        .into_view()
-                }
-                ValidationState::Valid => {
-                    view! {
-                        <div class=format!("") data-type="success">
-                            {validation_children.clone()}
-                        </div>
-                    }
-                        .into_view()
-                }
-                ValidationState::Dirty => view! { <></> }.into_view(),
-            }}
-
+            <div id={format!("{}-error error {}", name, error_class)} data-state={move || state().to_string()}>
+                |_| { error_message_view()}
+            </div>
+            <div class={format!("summary {}", summary_class)}>{summary}</div>
         </div>
     }
 }
