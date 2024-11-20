9717b8d5d9ffff26a1dbdb47405277adb982e7f0
Gavin Daly <gav.d@me.com>
2024-11-20 18:01:10 -0500

    style: refactor avatar component and shape styles
    Refactor the Avatar component to use `src` instead of `image` for 
    the image URL. Update shape styles in SCSS to remove the `:has` 
    pseudo-class for better compatibility. Enhance the documentation 
    with an example for the Avatar and add a width 
    property to allow for customizable avatar sizes.

--- src/components/avatar.rs
+++ src/components/avatar.rs
@@ -3,31 +3,34 @@ use leptos::*;
 
 /// A component to display an avatar image.
 ///
-/// # Arguments
-/// - `href` The URL of the image.
-/// - `name` The name of the person the avatar represents.
-/// - `shape` The shape of the avatar.
-/// - `color` The color of the avatar.
-/// - `label`
-/// - `icon`
+/// # Example
 ///
+/// ```rust
+/// use leptos::*;
+/// #[component]
+/// fn Example() -> impl IntoView {
+///     view! {
+///         <Avatar src="https://example.com/avatar.jpg"
+///             name="John Doe"
+///             shape="circle"
+///             width="150" />
+///     }
+/// }
+/// ```
 #[component]
 pub fn Avatar(
     ///href The URL of the image.
     #[prop(into)]
-    image: String,
+    src: String,
     /// The name of the person the avatar represents.
-    #[prop(optional)]
+    #[prop(into, optional)]
     name: Option<String>,
     #[prop(optional, into)] shape: Option<Shape>,
+    #[prop(default={"100".into()}, into)] width: String,
 ) -> impl IntoView {
     let alt = match name {
-        Some(name) => format!("avatar image for {name}",),
+        Some(name) => format!("Avatar for {name}",),
         None => "avatar image for unknown user".to_string(),
     };
-    view! {
-        <picture data-shape=shape>
-            <img href=image alt=alt/>
-        </picture>
-    }
+    view! { <img data-shape=shape src=src alt=alt width=width/> }
 }
