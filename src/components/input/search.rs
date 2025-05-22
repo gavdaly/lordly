47bd0315000d6c12b934a202ec4704bdd1395683
Gavin Daly <gav.d@me.com>
2025-05-22 00:38:37 -0400

    Enable search component and add URL support for hints

--- src/components/input/search.rs
+++ src/components/input/search.rs
@@ -1,44 +1,53 @@
 use leptos::ev::*;
-use leptos::form::*;
 use leptos::prelude::*;
-use leptos::server_fn::*;
 
 /// A search component.
-///
 #[component]
-pub fn Search(
-    #[prop(default=Callback::from(|_: String| vec![]), into)] hints: Callback<String, Vec<String>>,
+pub fn SearchBody(
+    #[prop(optional)] hints: Option<Callback<String, Vec<Hint>>>,
     #[prop(default="find".into(), into)] button: String,
-    // #[prop(into)] action: ServerAction<S>,
 ) -> impl IntoView {
-    let (auto_complete, set_auto_complete) = signal(None);
-    let changed = |ev: InputEvent| {
-        let Some(value) = ev.value_of().as_string() else {
+    let (auto_complete, set_auto_complete) = signal(None::<Vec<Hint>>);
+
+    let changed = move |ev: Event| {
+        let Some(hints) = hints else {
             return;
         };
-        if value.is_empty() {
+        let value = event_target_value(&ev);
+        if value.len() == 0 {
             return;
         };
-        set_auto_complete.set(Some(hints.run(value)));
+        let possible_hints = hints.run(value);
+        set_auto_complete.set(Some(possible_hints));
     };
 
     view! {
-        <form>
-        // <Action`Form action>
+        <>
             <label for="search">"Search"</label>
             <input type="search" id="search" name="q" on:change=changed/>
             <button type="submit">{button}</button>
-            <Show when=move || auto_complete.get().is_some()
-                children=move || {
-                    let hints = auto_complete.get().unwrap();
-                    view! {
-                        <ul>
-                            {hints.into_iter().map(|hint| view! { <li>{hint}</li> }).collect_view()}
-                        </ul>
-                    }
-                }
-            />
-        // </ActionForm>
-        </form>
+            <Show
+                when=move || auto_complete.get().is_some()
+            >
+                <ul>
+                    {move || {
+                        if let Some(hints) = auto_complete.get() {
+                            hints.into_iter()
+                                .map(|hint| view! { <li><a href={hint.url}>{hint.text}</a></li> })
+                                .collect_view().into_any()
+                        } else {
+                            view! {}.into_any()
+                        }
+                    }}
+
+                </ul>
+            </Show>
+        </>
     }
+}
+
+#[derive(Debug, Clone)]
+pub struct Hint {
+    text: String,
+    url: String,
 }
